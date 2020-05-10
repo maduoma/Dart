7a766470b923cf8a7084a6fc9cc0d87d84bbe018
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-10 13:25:42 +0100

    Hello

---
+++ hello_world/bin/main.dart
@@ -0,0 +1,14 @@
+class Hello {
+   void printHello(String welcome){
+     print(welcome);
+   }
+}
+  void main(List<String> arguments) {
+    Hello().printHello('Welcome to the world of Dart!');
+  }
+
+
+
+
+
+
