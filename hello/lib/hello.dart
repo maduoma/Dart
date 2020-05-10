50a679153ddaee93219f246241669587efe84c75
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-10 13:26:54 +0100

    Welcome  to dart

---
+++ hello/lib/hello.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+    Hello().printInteger(3, 4);
+}  
+class Hello {
+  void printInteger(int num1, int num2){
+    print('The sum is ${num1 + num2}.');
+  }
+}
+  
+  
