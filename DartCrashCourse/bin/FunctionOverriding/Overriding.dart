36278d54e5d3a527fca72b0dfda36c772d7439d3
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-29 10:25:42 +0100

    Overriding in Dart

---
+++ DartCrashCourse/bin/FunctionOverriding/Overriding.dart
@@ -0,0 +1,31 @@
+class Fish {
+  void move() {
+    print('Changed position...');
+  }
+}
+
+mixin CanFly {
+  void fly() {
+    print('Can fly');
+  }
+}
+
+mixin CanSwim {
+  void swim() {
+    print('Can swim');
+  }
+}
+
+class Duck extends Fish with CanFly, CanSwim {
+  @override
+  void move() {
+    super.move();
+    print('by flying.');
+  }
+}
+
+void main() {
+  Fish().move();
+  Duck().fly();
+  Duck().swim();
+}
