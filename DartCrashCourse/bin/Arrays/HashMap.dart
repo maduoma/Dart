e5ff33ffdd9950e6720f3bccdb762b7735898797
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-29 07:05:06 +0100

    Collection in Dart: HashMap

--- DartCrashCourse/bin/Arrays/HashMap.dart
+++ DartCrashCourse/bin/Arrays/HashMap.dart
@@ -1,20 +1,20 @@
 import 'dart:collection';
 
+import 'dart:io';
+
 class HashMapDemo {
   void myEmployees() {
     var myEmpl = HashMap<String, int>();
     myEmpl.putIfAbsent('Maduabughichi', () => 9000000000000000000);
     myEmpl.putIfAbsent('Judith', () => 9000000000000000000);
     for (var employees in myEmpl.keys) {
-      print(employees);
+      stdout.write('$employees with salary of \$'); //Prints to to the console without going to the next line
       print(myEmpl[employees]);
     }
-//    for (var employee in myEmpl.keys) {
-//      print('Employee name and salary: $employee : $myEmpl[employees].');
-//    }
-    print("==================OR=============================");
-    for (var employee in myEmpl.keys){
-      print('Employee name: ${employee.toUpperCase()} and employee salary: ${myEmpl[employee]}.');
+    print('==================OR=============================');
+    for (var employee in myEmpl.keys) {
+      print(
+          'Employee name: ${employee.toUpperCase()} and employee salary: ${myEmpl[employee]}.');
     }
   }
 }
