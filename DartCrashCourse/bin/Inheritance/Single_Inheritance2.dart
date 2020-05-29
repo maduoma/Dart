fd665e72de8bd7da7322daf811165fca1eff5d48
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-29 12:08:03 +0100

    Single_Inheritance2 in Dart

--- DartCrashCourse/bin/Inheritance/Single_Inheritance2.dart
+++ DartCrashCourse/bin/Inheritance/Single_Inheritance2.dart
@@ -1,11 +1,13 @@
 class Car {
   // Use => to function and its body in one line of code
-  void showOwner() => print('This is Parent\'s car');
+  void showOwner() => print(
+      'This is the parent\'s Car class from which other classes extended.');
 }
 
 class Jay extends Car {
   @override
   void showOwner() {
+    print('============Jay class extends Car class============');
     print('This is JD\'s car');
   }
 }
@@ -13,13 +15,17 @@ class Jay extends Car {
 class Salon extends Car {
   @override
   void showOwner() {
+    print('============Salon class extends Car class============');
     print('This is Salon\'s car');
   }
 }
 
 class Mercedes extends Salon {
+  //Extends Salon class that extended Car class
   @override
   void showOwner() {
+    print(
+        '============Mercedes class extends Salon class that extended Car class============');
     print('This is Mercedes\' car');
   }
 }
@@ -27,6 +33,7 @@ class Mercedes extends Salon {
 void main() {
   //Omit type annotations for local variables: Jay jdCar = new Jay();
   //Unnecessary new keyword. Open documentation: Jay jdCar = new Jay();
+  print('============parent Car class============');
   var parentCar = Car();
   parentCar.showOwner();
 
